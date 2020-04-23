6a90039c39cca9be18a3f94a303c5aaf8f998a47
Björn Allvin <bjorn@allvin.com>
2020-04-23 15:43:19 +0200

    Updates

---
+++ precache-manifest.233f5338b1974b90bed3be07d444e40f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf41a23e78393ac3c0725d2f33c1ee2e",
+    "url": "/docs/index.html"
+  },
+  {
+    "revision": "7f9b66790f2f7380e7b6",
+    "url": "/docs/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "3d0314d4a7f95a9b8128",
+    "url": "/docs/static/css/main.d49d735f.chunk.css"
+  },
+  {
+    "revision": "7f9b66790f2f7380e7b6",
+    "url": "/docs/static/js/2.01a29334.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/docs/static/js/2.01a29334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0314d4a7f95a9b8128",
+    "url": "/docs/static/js/main.84fa9989.chunk.js"
+  },
+  {
+    "revision": "3ec08dc67e5f35f60a8d",
+    "url": "/docs/static/js/runtime-main.c697452b.js"
+  }
+]);
